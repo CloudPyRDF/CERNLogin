b56f7be4fffe9a6565467fde481f7c8388389979
dawid.bialka@gmail.com <dawid.bialka@gmail.com>
2021-11-21 22:25:19 +0100

    adding kerberos command invocation to obtain token

--- CERNLogin/__init__.py
+++ CERNLogin/__init__.py
@@ -1,4 +1,17 @@
-from cern_login import LoginHandler
+from .cern_login import LoginHandler
+from pathlib import Path
+import json
+
+HERE = Path(__file__).parent.resolve()
+
+with (HERE / "labextension" / "package.json").open() as fid:
+    data = json.load(fid)
+
+def _jupyter_labextension_paths():
+    return [{
+        "src": "labextension",
+        "dest": data["name"]
+    }]
 
 def _jupyter_server_extension_points():
     return [{
@@ -6,5 +19,9 @@ def _jupyter_server_extension_points():
     }]
 
 def load_jupyter_server_extension(server_app):
-    handlers = [("/AWSConnector", LoginHandler)]
-    server_app.web_app.add_handlers(".*$", handlers)
+    #handlers = [("/CERNLogin", LoginHandler)]
+    #server_app.web_app.add_handlers(".*$", handlers)
+    pass
+    
+def _jupyter_server_extension_paths():
+    return [{"module": "CERNLogin"}]
